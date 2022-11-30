69492043502f2d7e892a28542513ce33bb9f5673
Matt <matthew.buckley@wsp.com>
2022-11-30 19:54:01 +0000

    Attempt to add LSOA map to dashboard WIP

--- dashboard/main.py
+++ dashboard/main.py
@@ -16,6 +16,7 @@ import requests
 import pyproj
 import pandas as pd
 import geopandas as gpd
+from shapely import geometry
 
 # Local imports
 
@@ -75,34 +76,91 @@ class TestDashboard:
 
 
 class DrawDashboard:
+    X_RANGE = (-342169, -323569)
+    Y_RANGE = (7055344, 7066390)
+
     def __init__(self) -> None:
         self.source = models.ColumnDataSource(dict(xs=[], ys=[]))
 
         p = plotting.figure(
-            x_range=(-342169, -323569),
-            y_range=(7055344, 7066390),
+            x_range=self.X_RANGE,
+            y_range=self.Y_RANGE,
             x_axis_type="mercator",
             y_axis_type="mercator",
+            sizing_mode="stretch_both",
         )
         p.add_tile(tile_providers.get_provider(tile_providers.CARTODBPOSITRON))
         r = p.multi_line("xs", "ys", source=self.source)
         tool = models.PolyDrawTool(renderers=[r])
         p.add_tools(tool)
 
-        columns = [
-            models.TableColumn(field="xs", title="X"),
-            models.TableColumn(field="ys", title="Y"),
-        ]
-        dt = models.DataTable(columns=columns, source=self.source)
+        self.load_lsoas()
+        # p.multi_polygons(
+        #     xs="xs", ys="ys", color="colour", alpha=0.5, source=self.lsoa_source
+        # )
 
-        but = models.Button(label="Click me")
+        dt = models.DataTable(
+            columns=[
+                models.TableColumn(field="xs", title="X"),
+                models.TableColumn(field="ys", title="Y"),
+            ],
+            source=self.source,
+        )
+        lsoa_dt = models.DataTable(
+            columns=[models.TableColumn(field="LSOA21CD", title="LSOA21CD")],
+            # source=self.lsoa_source,
+        )
+
+        but = models.Button(label="Update Graph", sizing_mode="stretch_both")
         but.on_click(self.button_click)
 
-        plotting.curdoc().add_root(layouts.column(but, p, dt))
+        self.mode_select = models.Select(
+            title="Mode",
+            value="walk",
+            options=["walk", "cycle"],
+            sizing_mode="stretch_both",
+        )
 
+        selections = layouts.row(but, self.mode_select, sizing_mode="stretch_width")
+        outputs = models.Tabs(
+            tabs=[
+                models.Panel(child=p, title="Map"),
+                models.Panel(child=dt, title="New Links"),
+                models.Panel(child=lsoa_dt, title="LSOA Data"),
+            ],
+        )
+
+        plotting.curdoc().add_root(
+            layouts.column(selections, outputs, sizing_mode="stretch_both")
+        )
+
+    def load_lsoas(self):
         self.boundaries = gpd.read_file(lsoas_shp)[["LSOA21CD", "geometry"]]
-        # self.boundaries = self.boundaries.to_crs("epsg:4326")
-        print(self.boundaries.head())
+
+        # Drop multipolygons
+        self.boundaries = self.boundaries.loc[self.boundaries.geometry.geom_type == "Polygon"]
+        
+        self.boundaries = self.boundaries.to_crs("epsg:4326")
+
+        t = pyproj.Transformer.from_crs("epsg:3857", "epsg:4326")
+        x, y = t.transform(self.X_RANGE, self.Y_RANGE)
+        clip_poly = geometry.Polygon(
+            [(y[0], x[0]), (y[1], x[0]), (y[1], x[1]), (y[1], x[0])]
+        )
+        self.boundaries = self.boundaries.clip(clip_poly)
+
+        self.boundaries.loc[:, "colour"] = "black"
+
+        self.lsoa_source = models.GeoJSONDataSource(geojson=self.boundaries.to_json())
+
+    def update_lsoas(self, data: pd.DataFrame):
+        updated = self.boundaries.merge(
+            data, left_on="LSOA21CD", right_on="lsoa", how="inner"
+        )
+        # TODO Calculate colour based on cmap
+        updated.loc[:, "colour"] = "yellow"
+
+        self.lsoa_source = models.GeoJSONDataSource(updated.to_json())
 
     def button_click(self):
         df = self.source.to_df()
@@ -116,7 +174,7 @@ class DrawDashboard:
 
         payload = {
             "lat_long_pairs": lat_long,
-            "mode_simpler": "walk",
+            "mode_simpler": self.mode_select.value,
             "TripStartHours": 8,
             "return_home": False,
             "max_travel_time": 3600,
@@ -124,7 +182,6 @@ class DrawDashboard:
             "arrival_times": (seconds_since_midnight(dt.time(8)),),
             "departure_times": (seconds_since_midnight(dt.time(8)),),
         }
-        pprint.pp(payload)
 
         res = requests.post(AC_URL, json=payload, verify=False)
         api_output = pd.DataFrame(res.json())
